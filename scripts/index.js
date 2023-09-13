5d4d4423cc91f9be6704b36aec4d6926383ce5bb
Mohammad Seydizadeh <immtrez@gmail.com>
2023-09-13 04:51:29 +0330

    nav - faq

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,13 @@
 // ** ELEMENTS
+const body = document.body;
 const video = document.querySelector('.device-video video');
 const videoPlayBtn = document.querySelector('.device-video .play-btn');
+const specificationsItems = document.querySelector('.specifications .items');
+const specificationsTrigger = document.querySelector('.specifications .more-btn');
+const faqItems = document.querySelectorAll('.faq-item');
+const faqTriggers = document.querySelectorAll('.faq-question');
+
+let lastScroll = 0;
 
 // VIDEO
 const handlePlayVideo = () => {
@@ -18,5 +25,61 @@ const handleVideoEnd = () => {
   }
 };
 
+// SPECIFICATIONS EXPAND
+const handleExpandCollapse = () => {
+  specificationsItems.classList.toggle('expanded');
+};
+
+// FAQ
+const handleToggleFaq = (e) => {
+  const clickedFaqItem = e.target.closest('.faq-item');
+  if (!clickedFaqItem) return;
+
+  const clickedFaqData = clickedFaqItem.dataset.faq;
+  const clickedFaqAnswer = clickedFaqItem.querySelector('.faq-answer');
+
+  faqItems.forEach((faqItem) => {
+    const dataFaq = faqItem.dataset.faq;
+    if (clickedFaqData === dataFaq) {
+      // open or close clicked item (based on prev state)
+      clickedFaqItem.classList.toggle('expanded');
+
+      if (clickedFaqAnswer.style.maxHeight) clickedFaqAnswer.style.maxHeight = null;
+      else clickedFaqAnswer.style.maxHeight = clickedFaqAnswer.scrollHeight + 'px';
+    } else {
+      // Close all other expanded items
+      faqItem.classList.remove('expanded');
+      const faqAnswer = faqItem.querySelector('.faq-answer');
+      if (faqAnswer.style.maxHeight) faqAnswer.style.maxHeight = null;
+    }
+  });
+};
+
 videoPlayBtn.addEventListener('click', handlePlayVideo);
 video.addEventListener('timeupdate', handleVideoEnd);
+
+specificationsTrigger.addEventListener('click', handleExpandCollapse);
+
+faqTriggers.forEach((faqTrigger) =>
+  faqTrigger.addEventListener('click', handleToggleFaq),
+);
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.scrollY;
+
+  if (currentScroll <= 0) {
+    body.classList.remove('scroll-up');
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+    body.classList.remove('scroll-up');
+    body.classList.add('scroll-down');
+  }
+
+  if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
+    body.classList.remove('scroll-down');
+    body.classList.add('scroll-up');
+  }
+
+  lastScroll = currentScroll;
+});
